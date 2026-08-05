6a3ab6990e58f7b2b2637af66599ed7ffae418fb
LeoHou_Monkey <leohou910@gmail.com>
2026-08-05 18:48:27 +0800

    chore: refact 9

--- src/main/java/me/aleksilassila/litematica/refactor/core/task/impl/FillTask.java
+++ src/main/java/me/aleksilassila/litematica/refactor/core/task/impl/FillTask.java
@@ -1,15 +1,19 @@
 package me.aleksilassila.litematica.refactor.core.task.impl;
 
 import me.aleksilassila.litematica.refactor.core.task.AbstractPrinterTask;
+import net.minecraft.core.BlockPos;
 
 public class FillTask extends AbstractPrinterTask {
+
     @Override
-    protected boolean canExcute() {
+    protected boolean canExecute(BlockPos pos) {
+        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    protected void excute() {
-       return;
+    protected void execute(BlockPos pos) {
+        // TODO Auto-generated method stub
+        
     }
 }
